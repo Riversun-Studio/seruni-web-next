e06484e5956bd32e0c020726cdb0450cd6970089
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-11 19:09:30 +0700

    improve meta tags (not final)

--- components/MetaTags/MetaTags.js
+++ components/MetaTags/MetaTags.js
@@ -14,26 +14,18 @@ const MetaTags = () => {
         We don’t work for you, but we want to work with you.
         Together, let’s conquer the digital world."
       />
-      <meta property="og:type" content="article" />
+      {/* Open Graph/Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://seruni-stagging-app.web.app/" />
       <meta property="og:title" content="Seruni" />
-      <meta
-        property="og:description"
-        content="We are SERUNI, a creative social media agency based in Bandung,
-        or wherever we like to work. We’re young, smart, and free!
-        Even the sky is not our limit.
-        We don’t work for you, but we want to work with you.
-        Together, let’s conquer the digital world."
-      />
-      <meta
-        name="twitter:card"
-        content="We are SERUNI, a creative social media agency based in Bandung,
-        or wherever we like to work. We’re young, smart, and free!
-        Even the sky is not our limit.
-        We don’t work for you, but we want to work with you.
-        Together, let’s conquer the digital world."
-      />
-      <meta name="twitter:site" content="Seruni" />
-      <meta name="twitter:creator" content="Seruni" />
+      <meta property="og:description" content="We are SERUNI, a creative social media agency based in Bandung, or wherever we like to work. We’re young, smart, and free! Even the sky is not our limit. We don’t work for you, but we want to work with you. Together, let’s conquer the digital world." />
+      <meta property="og:image" content="./img/seruni-sticker.webp" />
+      {/* Twitter */}
+      <meta property="twitter:card" content="./img/seruni-sticker.webp" />
+      <meta property="twitter:url" content="https://seruni-stagging-app.web.app/" />
+      <meta property="twitter:title" content="Seruni" />
+      <meta property="twitter:description" content="We are SERUNI, a creative social media agency based in Bandung, or wherever we like to work. We’re young, smart, and free! Even the sky is not our limit. We don’t work for you, but we want to work with you. Together, let’s conquer the digital world." />
+      <meta property="twitter:image" content="./img/seruni-sticker.webp" />
     </Head>
   )
 }
