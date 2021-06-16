21444804fc216aec8b534abb8560c3a725d0a352
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-16 16:23:47 +0700

    fix meta tags

--- components/MetaTags/MetaTags.js
+++ components/MetaTags/MetaTags.js
@@ -16,16 +16,16 @@ const MetaTags = () => {
       />
       {/* Open Graph/Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://seruni-stagging-app.web.app/" />
+      <meta property="og:url" content="https://serunicreative.com/" />
       <meta property="og:title" content="Seruni" />
       <meta property="og:description" content="We are SERUNI, a creative social media agency based in Bandung, or wherever we like to work. We’re young, smart, and free! Even the sky is not our limit. We don’t work for you, but we want to work with you. Together, let’s conquer the digital world." />
-      <meta property="og:image" content="./img/seruni-sticker.webp" />
+      <meta property="og:image" content="https://serunicreative.com/img/seruni-sticker.png" />
       {/* Twitter */}
-      <meta property="twitter:card" content="./img/seruni-sticker.webp" />
-      <meta property="twitter:url" content="https://seruni-stagging-app.web.app/" />
+      <meta property="twitter:card" content="https://serunicreative.com/img/seruni-sticker.png" />
+      <meta property="twitter:url" content="https://serunicreative.com/" />
       <meta property="twitter:title" content="Seruni" />
       <meta property="twitter:description" content="We are SERUNI, a creative social media agency based in Bandung, or wherever we like to work. We’re young, smart, and free! Even the sky is not our limit. We don’t work for you, but we want to work with you. Together, let’s conquer the digital world." />
-      <meta property="twitter:image" content="./img/seruni-sticker.webp" />
+      <meta property="twitter:image" content="https://serunicreative.com/img/seruni-sticker.png" />
     </Head>
   )
 }
